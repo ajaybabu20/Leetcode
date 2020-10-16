7f96c8575840907cb2a9288eb15adc3e7c1732a7
ajaybabu20 <ajay.babu@domain.com.au>
2020-10-16 18:12:55 +1100

    Feat: Two Sum problem with tests.

---
+++ solutions/TwoSum.py
@@ -0,0 +1,26 @@
+"""Leetcode challenge the description and details about the challenge can be
+found in the link https://leetcode.com/problems/two-sum/.
+
+"""
+from typing import List,Optional
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """To calculate the two sums challenge
+
+        Args:
+            nums: A given list of numbers
+            target: Target Number
+
+        Returns:
+            A list of index
+        """
+        if len(nums) == 2:
+            return [0, 1]
+        for index in range(len(nums)):
+            value = nums[index]
+            if target - value in nums:
+                if index != nums.index(target - value):
+                    return sorted([index, nums.index(target - value)])
+        return List[int]
